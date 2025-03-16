61147e0891fadb570c60b0f6e87e7414138d2b35
ukeem <ikim.trader@gmail.com>
2025-03-16 16:57:18 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,17 @@ import * as express from "express";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app
-        .enableCors
-        // 	{
-        //     origin: "http://localhost:3000", // ✅ Разрешаем запросы с фронтенда
-        //     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        //     allowedHeaders: "Content-Type,Authorization",
-        //     credentials: true, // Если используешь куки или сессии
-        // }
-        ();
+    app.enableCors({
+        origin: [
+            "https://top24.kz",
+            "https://www.top24.kz",
+            // "http://localhost:3000",
+        ], // Разрешаем запросы с фронтенда
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders: "Content-Type, Authorization", // Разрешаем использование заголовков
+        credentials: true, // Разрешаем передачу cookies (если нужно)
+    });
+
     app.setGlobalPrefix("api");
     // Делаем папку images доступной по /images
     app.use("/images", express.static(path.join(__dirname, "..", "images")));
