0e8e7e11514e4beb9aaf262c7b0b8d9a3575cc81
ukeem <ikim.trader@gmail.com>
2025-03-16 05:33:06 +0500

    Update tasks.service.ts

--- src/tasks.service.ts
+++ src/tasks.service.ts
@@ -1,16 +1,15 @@
 /** @format */
 
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
 import { PostsService } from "./posts/posts.service";
 
 @Injectable()
-export class TasksService {
+export class TasksService implements OnModuleInit {
     constructor(private readonly postsService: PostsService) {}
 
-    @Cron("*/30 * * * * *") // Запуск каждые 3 часа
-    async handleCron1() {
-        console.log("⏳ Запуск addPosts()...");
+    async onModuleInit() {
+        console.log("🚀 Приложение запущено — выполняем addPosts()");
         try {
             await this.postsService.addPosts();
             console.log("✅ addPosts() выполнен успешно.");
@@ -18,15 +17,29 @@ export class TasksService {
             console.error("❌ Ошибка в addPosts():", error);
         }
     }
-
-    @Cron("0 0 * * *") // Раз в день в полночь
-    async handleCron2() {
-        console.log("⏳ Запуск truncateQueries()...");
-        try {
-            await this.postsService.truncateQueries();
-            console.log("✅ truncateQueries() выполнен успешно.");
-        } catch (error) {
-            console.error("❌ Ошибка в truncateQueries():", error);
-        }
-    }
 }
+// export class TasksService {
+//     constructor(private readonly postsService: PostsService) {}
+
+//     @Cron("30 * * * * *") // Запуск каждые 3 часа
+//     async handleCron1() {
+//         console.log("⏳ Запуск addPosts()...");
+//         try {
+//             await this.postsService.addPosts();
+//             console.log("✅ addPosts() выполнен успешно.");
+//         } catch (error) {
+//             console.error("❌ Ошибка в addPosts():", error);
+//         }
+//     }
+
+//     @Cron("0 0 * * *") // Раз в день в полночь
+//     async handleCron2() {
+//         console.log("⏳ Запуск truncateQueries()...");
+//         try {
+//             await this.postsService.truncateQueries();
+//             console.log("✅ truncateQueries() выполнен успешно.");
+//         } catch (error) {
+//             console.error("❌ Ошибка в truncateQueries():", error);
+//         }
+//     }
+// }
